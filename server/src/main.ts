a0d48aaad8ecd8f0618f13ac6c679c6de50f1217
JAVA_09_21110856 <21110856@student.hcmute.edu.vn>
2024-10-07 23:19:36 +0700

    ffdsf

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+// main.ts (NestJS)
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -5,7 +6,13 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get<number>('PORT') || 3001;
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Thay đổi URL này theo frontend của bạn
+    credentials: true,
+  });
+
   await app.listen(port);
 }
 bootstrap();
